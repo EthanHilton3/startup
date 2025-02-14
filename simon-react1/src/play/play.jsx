60c70e5c98ae0759a142b135affd9cd6b114c53d
EthanHIlton <77414289+EthanHIlton@users.noreply.github.com>
2025-02-13 19:15:51 -0700

    inserted default react into jsx files

---
+++ simon-react1/src/play/play.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function play() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>play displayed here</div>
+    </main>
+  );
+}
