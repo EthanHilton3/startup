8c1537e5c1b2f222a1385160f8f710bf4853106b
EthanHIlton <77414289+EthanHIlton@users.noreply.github.com>
2025-02-13 22:54:05 -0700

    converted play to react

--- startup-react-pt1/src/play/play.jsx
+++ startup-react-pt1/src/play/play.jsx
@@ -3,30 +3,30 @@ import './play.css';
 
 export function Play() {
   return (
-    <main className="bg-secondary">
+    <main className="flex">
       <div className="players">
         Player
         <span className="player-name">Mystery player</span>
         <div id="player-messages">
-          <div className="event"><span className="player-event">Linus</span> scored 377</div>
-          <div className="event"><span className="player-event">Linus</span> started a new game</div>
-          <div className="event"><span className="system-event">game</span> connected</div>
+          <div className="event"><span className="player-event">Linus</span> started clicking</div>
+          <div className="event"><span className="player-event">Linus</span> started clicking</div>
+          <div className="event"><span className="system-event">game</span> got 337 clicks!</div>
         </div>
       </div>
 
-      <div className="game">
-        <div className="button-container">
-          <button className="button-top-left"></button>
-          <button className="button-top-right"></button>
-          <button className="button-bottom-left"></button>
-          <button className="button-bottom-right"></button>
-          <div className="controls center">
-            <div className="game-name">Simon<sup>&reg;</sup></div>
-            <div className="score center">--</div>
-            <button className="btn btn-primary">Reset</button>
-          </div>
-        </div>
+      <div className="score">1,000,000,000,000</div>
+      <hr />
+      <div className="game flex">
+        <button className="btn btn-primary">
+          <img src="cosmo_the_cougar_machine_embroidery_design.jpg" />
+        </button>
       </div>
+
+      <div className="quote-box flex text-center">
+        <div className="quote">"Believe it!!"</div>
+        <div className="author">Naruto Uzumaki</div>
+      </div>
+
     </main>
   );
 }
