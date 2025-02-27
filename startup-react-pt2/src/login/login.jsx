50bfbe0d3f4da3dfc402b3055adab4661fa524b1
EthanHIlton <77414289+EthanHIlton@users.noreply.github.com>
2025-02-26 18:28:29 -0700

    converting login: header now appears

--- startup-react-pt2/src/login/login.jsx
+++ startup-react-pt2/src/login/login.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
-export function Login() {
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
+
+export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className="flex text-center">
       <div>
-        <h1>Welcome to Cougar Clicker</h1>
-        <p>Become the clicker champion!</p>
+        {authState !== AuthState.Unknown && <h1>Welcome to Cougar Clicker</h1>}
+        {authState !== AuthState.Unknown && <p>Become the clicker champion!</p>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
+      </div>
+      <div>
         <form method="get" action="play.html">
           <div className="input-group mb-3">
             <input className="form-control" type="text" placeholder="cosmo_the_cougar@byu.edu" />
