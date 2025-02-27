fdd17182a2705862ad8789e3f7d9f7051c51b403
EthanHIlton <77414289+EthanHIlton@users.noreply.github.com>
2025-02-27 11:34:42 -0700

    trying to save my scores in local storage so they appear on the scores page

--- startup-react-pt2/src/play/game.jsx
+++ startup-react-pt2/src/play/game.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
 
 import { Button } from 'react-bootstrap';
-import { CosmoButton } from './CosmoButton';
-import { delay } from './delay';
 import { GameEvent, GameNotifier } from './gameNotifier';
 import './play.css';
 
-export function Game({ userName }) {
+export function Game(props) {
+    const userName = props.userName;
     const [count, setCount] = React.useState(parseInt(localStorage.getItem('count')) || 0);
 
     function incrementCount() {
         setCount(count + 1);
         localStorage.setItem('count', count + 1);
+
+        const date = new Date().toLocaleDateString();
+        const newScore = { name: userName, score: score, startDate: date, currentDate: date};
+        updateScoresLocal(newScore);
+
+        if ((count + 1) % 25 === 0) {
+            GameNotifier.broadcastEvent(userName, GameEvent.End, newScore);
+        }
     }
+
+    function updateScoresLocal(newScore) {
+        let scores = [];
+        const scoresText = localStorage.getItem('scores');
+        if (scoresText) {
+          scores = JSON.parse(scoresText);
+        }
+    
+        let found = false;
+        for (const [i, prevScore] of scores.entries()) {
+          if (newScore.score > prevScore.score) {
+            scores.splice(i, 0, newScore);
+            found = true;
+            break;
+          }
+        }
+    
+        if (!found) {
+          scores.push(newScore);
+        }
+    
+        if (scores.length > 10) {
+          scores.length = 10;
+        }
+    
+        localStorage.setItem('scores', JSON.stringify(scores));
+      }
 
     return (
         <div className='game'>
