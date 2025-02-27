76d1d60c4fbbd2f36b814cef5b1391fe871f8c9a
EthanHIlton <77414289+EthanHIlton@users.noreply.github.com>
2025-02-27 12:46:12 -0700

    fixed why my username was always undefined

--- startup-react-pt2/src/app.jsx
+++ startup-react-pt2/src/app.jsx
@@ -67,8 +67,7 @@ export default function App() {
                         }
                         exact
                     />
-                    <Route path='/' element={<Login />} exact />
-                    <Route path='/play' element={<Play />} />
+                    <Route path='/play' element={<Play userName={userName} />} />
                     <Route path='/scores' element={<Scores />} />
                     <Route path='/about' element={<About />} />
                     <Route path='*' element={<NotFound />} />
