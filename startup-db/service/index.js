23142f0d4f6cb0b489067189f54ff727592293d4
EthanHIlton <77414289+EthanHIlton@users.noreply.github.com>
2025-04-12 12:28:00 -0600

    updating index.js: includes database.js and no longer uses its own data structures for data

--- startup-db/service/index.js
+++ startup-db/service/index.js
@@ -3,12 +3,9 @@ const bcrypt = require('bcryptjs');
 const express = require('express');
 const uuid = require('uuid');
 const app = express();
+const DB = require('./database.js');
 
 const authCookieName = 'token';
-
-// The scores and users are saved in memory and disappear whenever the service is restarted.
-let users = [];
-let scores = [];
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
