5d3c9ac2d461178bf4384fac5a7ae0fd4cc3eddf
EthanHIlton <77414289+EthanHIlton@users.noreply.github.com>
2025-04-12 12:30:09 -0600

    delete now uses DB.updateUser

--- startup-db/service/index.js
+++ startup-db/service/index.js
@@ -55,6 +55,7 @@ apiRouter.delete('/auth/logout', async (req, res) => {
   const user = await findUser('token', req.cookies[authCookieName]);
   if (user) {
     delete user.token;
+    DB.updateUser(user);
   }
   res.clearCookie(authCookieName);
   res.status(204).end();
